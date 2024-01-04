7a897757ddea7f2009aebfaebc65e48021ef0771
石田沙有里 <a0r1ng0@icloud.com>
2024-01-05 08:42:17 +0900

    miner change

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -30,86 +30,89 @@
     {{-- @endif --}}
 
     {{-- Seller Sidebar --}}
-    {{-- @if (request()->is('Seller/*')) --}}
-    <div class="col-3">
+    @if (request()->is('Seller/*'))
+        <div class="col-3">
 
-        <div class="list-group">
-            <span class="list-group-item">
-                <h3>Seller Navigation</h3>
-            </span>
-            {{-- <a href="{{ route('seller.mypage') }}" --}}
-            <a href="#" class="list-group-item {{ request()->is('seller.mypage') ? 'active' : '' }}">
-                My Page
-            </a>
-            {{-- <a href="{{ route('seller.ordermanagement') }}" --}}
-            <a href="#" class="list-group-item {{ request()->is('seller.ordermanagement') ? 'active' : '' }}">
-                Order Management
-            </a>
-            {{-- <a href="{{ route('seller.salesmanagement') }}" --}}
-            <a href="#" class="list-group-item {{ request()->is('seller.salesmanagement') ? 'active' : '' }}">
-                Sales Management
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('seller.directmessages') ? 'active' : '' }}">
-                Direct Messages
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('seller.ranking') ? 'active' : '' }}">
-                Ranking
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('seller.productslist') ? 'active' : '' }}">
-                Products List
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('seller.draftlist') ? 'active' : '' }}">
-                Draft List
-            </a>
+            <div class="list-group">
+                <span class="list-group-item">
+                    <h3>Seller Navigation</h3>
+                </span>
+                {{-- <a href="{{ route('seller.mypage') }}" --}}
+                <a href="#" class="list-group-item {{ request()->is('seller.mypage') ? 'active' : '' }}">
+                    My Page
+                </a>
+                {{-- <a href="{{ route('seller.ordermanagement') }}" --}}
+                <a href="#" class="list-group-item {{ request()->is('seller.ordermanagement') ? 'active' : '' }}">
+                    Order Management
+                </a>
+                {{-- <a href="{{ route('seller.salesmanagement') }}" --}}
+                <a href="#" class="list-group-item {{ request()->is('seller.salesmanagement') ? 'active' : '' }}">
+                    Sales Management
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('seller.directmessages') ? 'active' : '' }}">
+                    Direct Messages
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('seller.ranking') ? 'active' : '' }}">
+                    Ranking
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('seller.productslist') ? 'active' : '' }}">
+                    Products List
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('seller.draftlist') ? 'active' : '' }}">
+                    Draft List
+                </a>
+            </div>
         </div>
-    </div>
-    {{-- @endif --}}
+    @endif
 
     {{-- Admin Sidebar --}}
-    {{-- @if (request()->is('admin/*')) --}}
-    <div class="col-3">
-        <div class="list-group">
-            <span class="list-group-item">
-                <h3>Admin Navigation</h3>
-            </span>
-            {{-- <a href="{{ route('admin.usermanagement') }}" --}}
-            <a href="#" class="list-group-item {{ request()->is('admin/usermanagement') ? 'active' : '' }}">
-                User Management
-            </a>
-            {{-- <a href="{{ route('admin.contentmanagement') }}" --}}
-            <a href="#" class="list-group-item {{ request()->is('admin/contentmanagement') ? 'active' : '' }}">
-                Content Management
-            </a>
-            {{-- <a href="{{ route('admin.complainhandling') }}" --}}
-            <a href="#" class="list-group-item {{ request()->is('admin/complainhandling') ? 'active' : '' }}">
-                Complain Handling
-            </a>
-            <a href="#"
-                class="list-group-item {{ request()->is('admin/connectionwithbuyerseller') ? 'active' : '' }}">
-                Connection With Buyer/Seller
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('admin/categoryadddelete') ? 'active' : '' }}">
-                Category Add/Delete
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('admin/notification') ? 'active' : '' }}">
-                Notification
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('admin/navbardropdown') ? 'active' : '' }}">
-                Navbar/Dropdown
-            </a>
-            <a href="#"
-                class="list-group-item {{ request()->is('admin/followerfollowinglist') ? 'active' : '' }}">
-                Follower/Following List
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('admin/dashboard') ? 'active' : '' }}">
-                Dashboard
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('admin/ranking') ? 'active' : '' }}">
-                Ranking
-            </a>
-            <a href="#" class="list-group-item {{ request()->is('admin/customization') ? 'active' : '' }}">
-                Customization
-            </a>
+    @if (request()->is('admin/*'))
+        <div class="col-3">
+            <div class="list-group">
+                <span class="list-group-item">
+                    <h3>Admin Navigation</h3>
+                </span>
+                {{-- <a href="{{ route('admin.usermanagement') }}" --}}
+                <a href="#" class="list-group-item {{ request()->is('admin/usermanagement') ? 'active' : '' }}">
+                    User Management
+                </a>
+                {{-- <a href="{{ route('admin.contentmanagement') }}" --}}
+                <a href="#"
+                    class="list-group-item {{ request()->is('admin/contentmanagement') ? 'active' : '' }}">
+                    Content Management
+                </a>
+                {{-- <a href="{{ route('admin.complainhandling') }}" --}}
+                <a href="#"
+                    class="list-group-item {{ request()->is('admin/complainhandling') ? 'active' : '' }}">
+                    Complain Handling
+                </a>
+                <a href="#"
+                    class="list-group-item {{ request()->is('admin/connectionwithbuyerseller') ? 'active' : '' }}">
+                    Connection With Buyer/Seller
+                </a>
+                <a href="#"
+                    class="list-group-item {{ request()->is('admin/categoryadddelete') ? 'active' : '' }}">
+                    Category Add/Delete
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('admin/notification') ? 'active' : '' }}">
+                    Notification
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('admin/navbardropdown') ? 'active' : '' }}">
+                    Navbar/Dropdown
+                </a>
+                <a href="#"
+                    class="list-group-item {{ request()->is('admin/followerfollowinglist') ? 'active' : '' }}">
+                    Follower/Following List
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('admin/dashboard') ? 'active' : '' }}">
+                    Dashboard
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('admin/ranking') ? 'active' : '' }}">
+                    Ranking
+                </a>
+                <a href="#" class="list-group-item {{ request()->is('admin/customization') ? 'active' : '' }}">
+                    Customization
+                </a>
+            </div>
         </div>
-    </div>
-    {{-- @endif --}}
+    @endif
