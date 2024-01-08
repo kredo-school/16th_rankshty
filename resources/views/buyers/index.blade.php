5fab822c37c6d8f9a5ad2dae29c1b2f90d67a436
Nobuaki Yagi <m.yaginobuaki@gmail.com>
2024-01-08 22:03:33 +0900

    [hotfix/auth-ui] - Adding Auth and Bootstrap

---
+++ resources/views/buyers/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('title', 'Users')
+
+@section('content')
+<div class="container">
+    <div class="header">
+        <p>Account Infomation</p>
+    </div>
+    <div class="body">
+        <form action="#">
+            <p>First Name</p>
+            <p>David</p>
+
+            <p>Last Name</p>
+            <p>Miller</p>
+
+            <p>Company Name</p>
+            <p>NovaLink Global</p>
+
+            <p>Email</p>
+            <p>novaLink_global21@Inc.com</p>
+
+            <p>Phone Number</p>
+            <p>(603) 926-0167</p>
+
+            <p>Street Addres</p>
+            <p>4140 Parl</p>
+
+            <img src="" alt="">
+
+            <p>Country / Region</p>
+            <p>United States <i class="fa-solid fa-angle-down"></i></p>
+
+            <p>States</p>
+            <p>Washinton DC <i class="fa-solid fa-angle-down"></i></p>
+
+            <p>Zip Code</p>
+            <p>20033</p>
+
+            <p>password</p>
+            <p>**********</p>
+
+            <p>Credit Card Number</p>
+            <p>***************</p>
+
+            <p>Add review</p>
+            <p>Lorem ipsum dolor sit amet. Ut rerum asperiores et eveniet mollitia sit optio repellendus non minima quam vel obcaecati eius sed repudiandae tempora non dicta unde. Et dignissimos recusandae ut sint blanditiis et natus dolores qui facere suscipit? Eos voluptates eveniet et aliquam nesciunt vel excepturi enim aut adipisci culpa ut distinctio quia.
+                Et quia sapiente eos rerum accusantium et distinctio eveniet. Ab quos accusantium quo cupiditate iure aut rerum ipsa aut quia doloribus ut dolorem nihil et laborum eius aut fuga tempore. Non autem odit ut quod quos non corrupti quas et odit quas in eaque magni. In vero amet et ipsum sunt eum alias dicta cum dolorem magni non debitis reiciendis.
+                Aut quis omnis non voluptatem assumenda quo earum galisum cum ipsum corrupti ea tempora repellendus. Eos debitis dolore At magnam dolor aut nostrum veniam et magnam praesentium aut incidunt quia.</p>
+        </div>
+
+        <input type="button" name="#" value="Edit">
+    </form>
+</div>
+@endsection
