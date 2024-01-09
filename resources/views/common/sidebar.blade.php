73c3403a2cb8a17b342985302f82a9981e4adcf8
石田沙有里 <a0r1ng0@icloud.com>
2024-01-09 21:05:05 +0900

    [UI/common/sidebar] - Sidebar Conflict

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -25,78 +25,76 @@
     {{-- @endif --}}
 
     {{-- Seller Sidebar --}}
-    {{-- @if (request()->is('Seller/*')) --}}
-    <div class="col-3">
+    @if (request()->is('Seller/*'))
+        <div class="col-3">
 
-        <div class="list-group">
-            <span class="list-group-item">
-                <h3>Seller Navigation</h3>
-            </span>
-            <a href="#" class="list-group-item">
-                My Page
-            </a>
-            <a href="#" class="list-group-item">
-                Order Management
-            </a>
-            <a href="#" class="list-group-item">
-                Sales Management
-            </a>
-            <a href="#" class="list-group-item">
-                Direct Messages
-            </a>
-            <a href="#" class="list-group-item">
-                Ranking
-            </a>
-            <a href="#" class="list-group-item">
-                Products List
-            </a>
-            <a href="#" class="list-group-item">
-                Draft List
-            </a>
-        </div>
-    </div>
-    {{-- @endif --}}
+            <div class="list-group">
+                <span class="list-group-item">
+                    <h3>Seller Navigation</h3>
+                </span>
+                <a href="#" class="list-group-item">
+                    My Page
+                </a>
+                <a href="#" class="list-group-item">
+                    Order Management
+                </a>
+                <a href="#" class="list-group-item">
+                    Sales Management
+                </a>
+                <a href="#" class="list-group-item">
+                    Direct Messages
+                </a>
+                <a href="#" class="list-group-item">
+                    Ranking
+                </a>
+                <a href="#" class="list-group-item">
+                    Products List
+                </a>
+                <a href="#" class="list-group-item">
+                    Draft List
+                </a>
+            </div>
+            {{-- @endif --}}
 
-    {{-- Admin Sidebar --}}
-    {{-- @if (request()->is('admin/*')) --}}
-    <div class="col-3">
-        <div class="list-group">
-            <span class="list-group-item">
-                <h3>Admin Navigation</h3>
-            </span>
-            <a href="#" class="list-group-item">
-                User Management
-            </a>
-            <a href="#" class="list-group-item">
-                Content Management
-            </a>
-            <a href="#" class="list-group-item">
-                Complain Handling
-            </a>
-            <a href="#" class="list-group-item">
-                Connection With Buyer/Seller
-            </a>
-            <a href="#" class="list-group-item">
-                Category Add/Delete
-            </a>
-            <a href="#" class="list-group-item">
-                Notification
-            </a>
-            <a href="#" class="list-group-item">
-                Navbar/Dropdown
-            </a>
-            <a href="#" class="list-group-item">
-                Follower/Following List
-            </a>
-            <a href="#" class="list-group-item">
-                Dashboard
-            </a>
-            <a href="#" class="list-group-item">
-                Ranking
-            </a>
-            <a href="#" class="list-group-item">
-                Customization
-            </a>
-        </div>
-    </div>
-    {{-- @endif --}}
+            {{-- Admin Sidebar --}}
+            {{-- @if (request()->is('admin/*')) --}}
+            <div class="col-3">
+                <div class="list-group">
+                    <span class="list-group-item">
+                        <h3>Admin Navigation</h3>
+                    </span>
+                    <a href="#" class="list-group-item">
+                        User Management
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Content Management
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Complain Handling
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Connection With Buyer/Seller
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Category Add/Delete
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Notification
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Navbar/Dropdown
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Follower/Following List
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Dashboard
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Ranking
+                    </a>
+                    <a href="#" class="list-group-item">
+                        Customization
+                    </a>
+                </div>
+    @endif
