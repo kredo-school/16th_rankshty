649c0e9090d163e4bfbd4248f52fe8878e1c7277
Ayaka Suzuki <graphite0505@gmail.com>
2024-01-09 20:06:37 +0900

    [UI/seller/guidepage] - Adding Auth and Bootstrap
    Changes:
    - Added SellerController
    - Added Fontawesome
    - Merged main to this branch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    {{-- FONTAWESOME CDN --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <div id="app">
