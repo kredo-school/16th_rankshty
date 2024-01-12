07171c224a2ba2b976a3ca7c87cb51f3b0069e59
yyoshifumi <yaki_vic_777@yahoo.co.jp>
2024-01-12 19:43:51 +0900

    [UI/common/footer] - first commit

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="container-fluid navy-bg text-light py-4">
+  <div class="container">
+      <div class="row">
+          <div class="col-6">
+              <h5>About Rankshty</h5>
+              <p class="grayed-out">
+                Morbi cursus porttitor enim lobortis molestie.<br>
+                Duis gravida turpis dui, eget bibendum magna<br>
+                congue nec.<br>
+              <p>(219) 555-0114 <span class="grayed-out">or</span> Proxy@gmail.com</p>           
+          </div>
+          <div class="col-6 text-right">
+              <a href="#">Helps</a>
+          </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-9 text-center grayed-out">
+          <p>Ecobazar eCommerce © 2021. All Rights Reserved</p>
+        </div>
+        <div class="col-3">
+          <img src="http://127.0.0.1:8000/images/visa.png" alt="">
+          <img src="http://127.0.0.1:8000/images/mastercard.png" alt="">
+          <img src="http://127.0.0.1:8000/images/SecurePayment.png" alt="">
+
+          
+
+        </div>
+      </div>
+  </div>
+</footer>
