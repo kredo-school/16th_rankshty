2fb5dafc0a21659b8fcbd5e10c56ffc6e6e67393
1891kaoritanaka <adem_9s@infoseek.jp>
2024-01-13 18:59:53 +0900

    [UI/buyer/favorite-page] - removed the excess.

--- routes/web.php
+++ routes/web.php
@@ -18,36 +18,3 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/favorite', [App\Http\Controllers\HomeController::class, 'favorite'])->name('favorite');
-// ↑ change buyer only
-
-// Route::group(['middleware' => 'auth'],function(){
-//     Route::get('/', [HomeController::class, 'index'])->name('index');
-
-// });
-
-
-
-// Route::group(['middleware' => 'auth'],function(){
-//     Route::get('/', [HomeController::class, 'index'])->name('index');
-//     # Post
-//     Route::get('/post/create', [PostController::class,'create'])->name('post.create');
-
-//     #Admin
-//     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'],function(){
-//         # Admin Users
-//         //route('admin.users) is the same with admin/users
-//         Route::get('/users',[UsersController::class,'index'])->name('users');
-//         Route::delete('/users/{id}/deactivate',[UsersController::class,'deactivate'])->name('users.deactivate');
-//         # Admin Posts
-//         Route::get('/posts',[PostsController::class,'index'])->name('posts'); //admin.posts
-//         Route::delete('/posts/{id}/hide',[PostsController::class,'hide'])->name('posts.hide');
-//         # Admin Categories
-//         Route::get('/categories',[CategoriesController::class,'index'])->name('categories'); //admin.categories
-//         Route::delete('/categories/{id}/destroy',[CategoriesController::class,'destroy'])->name('categories.destroy');
-//     });
-
-//     Route::group(['middleware' => 'auth'], function(){
-//         Route::get('/',[HomeController::class, 'index'])->name('index');
-//         Route::get('/people',[HomeController::class, 'search'])->name('search');
-//     });
-// });
