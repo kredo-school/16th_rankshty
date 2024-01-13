bf1c5d5cb4e13867f301c11152b10d52c1130293
Ayaka Suzuki <graphite0505@gmail.com>
2024-01-13 18:00:27 +0900

    [UI/buyer/help-page] - Added helppage.blade.php

---
+++ app/Http/Controllers/BuyerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BuyerController extends Controller
+{
+    public function service()
+    {
+        return view('users.buyers.terms_of_service');
+    }
+
+    public function help()
+    {
+        return view('users.buyers.help');
+    }
+}
