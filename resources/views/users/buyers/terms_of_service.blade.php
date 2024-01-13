38adbb4a457cc116a8e23fbe1f68def60199ff9c
Ayaka Suzuki <graphite0505@gmail.com>
2024-01-13 21:16:49 +0900

    This part is [UI/buyer/terms-of-service], but  I can't change it. That's why I tried to modify it inside guidepage.

--- resources/views/users/buyers/terms_of_service.blade.php
+++ resources/views/users/buyers/terms_of_service.blade.php
@@ -1,145 +1,79 @@
 @extends('layouts.app')
 
-@section('title', 'Help page')
+@section('title','Terms of Service')
 
 @section('content')
-    <form action="#" method="get">
-        @csrf
+<form action="#" method="post">
+  @csrf
 
-        <div class="container">
+  <div class="container">
+    <div class="row">
+        <div class="col-6 mt-3">
+                <h2>100% Trusted <br> Organic Food Store</h2>
+                <p class="text-muted">
+                    Morbi porttitor ligula in nunc varius sagittis. Proin dui nisi, laoreet ut tempor ac, cursus vitae eros. Cras quis ultricies elit. Proin ac lectus arcu. Maecenas aliquet vel tellus at accumsan. Donec a eros non massa vulputate ornare. Vivamus ornare commodo ante, at commodo felis congue vitae.Morbi porttitor ligula in nunc varius sagittis. Proin dui nisi, laoreet ut tempor ac, cursus vitae eros. Cras quis ultricies elit. Proin ac lectus arcu. Maecenas aliquet vel tellus at accumsan. Donec a eros non massa vulputate ornare. Vivamus ornare commodo ante, at commodo felis congue vitae.Morbi porttitor ligula in nunc varius sagittis. Proin dui nisi, laoreet ut tempor ac, cursus vitae eros. 
+                </p>
+                <div class="text-center">
+                    <img src="{{ asset('assets/pexels-pixabay-264771.jpg') }}" style="width: 90%; height: 400px;  margin-top: 90px;" alt="picture box">
+                </div>
+        </div>
+    
+        <div class="col-6 mt-3">
+            <div class="text-center">
+                <img src="{{ asset('assets/pexels-photomix-company-230544.jpg') }}"  style="width: 90%; height: 300px: " alt="picture phone">
+            </div>
+            <h2 style="margin-top: 30px">100% Trusted <br> Organic Food Store</h2>
+            <p class="text-muted">Pellentesque a ante vulputate leo porttitor luctus sed eget eros. Nulla et rhoncus     neque. Duis non diam eget est luctus tincidunt a a mi. Nulla eu eros consequat tortor tincidunt feugiat. 
+            </p>
             <div class="row">
-                <div class="title fw-normal">
-                    <h1>
-                        Inquiry & FAQs
-                    </h1>
-                </div>
-                <div class="col-6 list-group">
-                    <div class="accordion mt-3" id="questionOne">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingOne">
-                                <button class="accordion-button" type="button" style="background-color: #6c757d1f;"
-                                    data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
-                                    aria-controls="collapseOne">
-                                    Question No.1
-                                </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                                data-bs-parent="#questionOne">
-                                <div class="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default,
-                                    until the collapse plugin adds the appropriate classes that we use to style each
-                                    element. These classes control the overall appearance, as well as the showing and
-                                    hiding via CSS transitions. You can modify any of this with custom CSS or
-                                    overriding our default variables. It's also worth noting that just about any HTML
-                                    can go within the though the transition does limit
-                                    overflow.
-                                </div>
-                            </div>
-                        </div>
+                <div class="col-6 mt-2">
+                    <div style="display: flex; align-items: center;">
+                        <i class="fa-solid fa-leaf" style="font-size: 25px; background-color: rgba(76, 160, 212, 0.10); padding: 15px; color: #0C66A6;  border-radius: 40%;"></i>
+                        <div style="margin-left: 10px; line-height: 1;">
+                            <h5 style="margin-bottom: 0;">100% Organic food</h5>
+                            <span class="text-muted">100% healthy & Fresh food.</span>
+                        </div>                        
                     </div>
-                    <div class="accordion mt-3" id="questionTwo">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingTwo">
-                                <button class="accordion-button" type="button" style="background-color: #6c757d1f;"
-                                    data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true"
-                                    aria-controls="collapseTwo">
-                                    Question No.2
-                                </button>
-                            </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo"
-                                data-bs-parent="#questionTwo">
-                                <div class="accordion-body">
-                                    <strong>This is the second item's accordion body.</strong> It is shown by default,
-                                    until the collapse plugin adds the appropriate classes that we use to style each
-                                    element. These classes control the overall appearance, as well as the showing and
-                                    hiding via CSS transitions. You can modify any of this with custom CSS or
-                                    overriding our default variables. It's also worth noting that just about any HTML
-                                    can go within the though the transition does limit
-                                    overflow.
-                                </div>
-                            </div>
-                        </div>
+                    <div style="display: flex; align-items: center;  margin-top: 10px;">
+                        <i class="fa-brands fa-shopify" style="font-size: 30px; background-color: rgba(76, 160, 212, 0.10); padding: 15px; color: #0C66A6;  border-radius: 40%;"></i>
+                        <div style="margin-left: 10px; line-height: 1;">
+                            <h5 style="margin-bottom: 0;">Customer Feedback</h5>
+                            <span class="text-muted">Our happy customers.</span>
+                        </div>                          
                     </div>
-                    <div class="accordion mt-3" id="questionThree">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingThree">
-                                <button class="accordion-button" type="button" style="background-color: #6c757d1f;"
-                                    data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true"
-                                    aria-controls="collapseThree">
-                                    Question No.3
-                                </button>
-                            </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree"
-                                data-bs-parent="#questionThree">
-                                <div class="accordion-body">
-                                    <strong>This is the second item's accordion body.</strong> It is shown by default,
-                                    until the collapse plugin adds the appropriate classes that we use to style each
-                                    element. These classes control the overall appearance, as well as the showing and
-                                    hiding via CSS transitions. You can modify any of this with custom CSS or
-                                    overriding our default variables. It's also worth noting that just about any HTML
-                                    can go within the  though the transition does limit
-                                    overflow.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="accordion mt-3" id="questionFour">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingFour">
-                                <button class="accordion-button" type="button" style="background-color: #6c757d1f;"
-                                    data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true"
-                                    aria-controls="collapseFour">
-                                    Question No.4
-                                </button>
-                            </h2>
-                            <div id="collapseFour" class="accordion-collapse collapse show" aria-labelledby="headingFour"
-                                data-bs-parent="#questionFour">
-                                <div class="accordion-body">
-                                    <strong>This is the second item's accordion body.</strong> It is shown by default,
-                                    until the collapse plugin adds the appropriate classes that we use to style each
-                                    element. These classes control the overall appearance, as well as the showing and
-                                    hiding via CSS transitions. You can modify any of this with custom CSS or
-                                    overriding our default variables. It's also worth noting that just about any HTML
-                                    can go within the though the transition does limit
-                                    overflow.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="accordion mt-3" id="questionFive">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingFive">
-                                <button class="accordion-button" type="button" style="background-color: #6c757d1f;"
-                                    data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true"
-                                    aria-controls="collapseFive">
-                                    Question No.5
-                                </button>
-                            </h2>
-                            <div id="collapseFive" class="accordion-collapse collapse show" aria-labelledby="headingFive"
-                                data-bs-parent="#questionFive">
-                                <div class="accordion-body">
-                                    <strong>This is the second item's accordion body.</strong> It is shown by default,
-                                    until the collapse plugin adds the appropriate classes that we use to style each
-                                    element. These classes control the overall appearance, as well as the showing and
-                                    hiding via CSS transitions. You can modify any of this with custom CSS or
-                                    overriding our default variables. It's also worth noting that just about any HTML
-                                    can go within the though the transition does limit
-                                    overflow.
-                                </div>
-                            </div>
-                        </div>
+                    <div style="display: flex; align-items: center;">
+                        <i class="fa-solid fa-headphones" style="font-size: 30px; margin-top: 10px;  background-color: rgba(76, 160, 212, 0.10); padding: 15px; color: #0C66A6;  border-radius: 40%;"></i>
+                        <div style="margin-left: 10px; line-height: 1;">
+                            <h5 style="margin-bottom: 0;">Free Shipping</h5>
+                            <span class="text-muted">Free shipping with discount.</span>
+                        </div>                          
                     </div>
                 </div>
-                <div class="col-6">
-                    <div class="text-center">
-                        <img src="{{ asset('assets/pexels-leeloo-thefirst-5428830.jpg') }}"  style="width: 90%; height: 300px: " alt="picture question-mark">
+                <div class="col-6 mt-2">
+                    <div style="display: flex; align-items: center;">
+                        <i class="fa-solid fa-box" style="font-size: 30px; background-color: rgba(76, 160, 212, 0.10); padding: 15px; color: #0C66A6;  border-radius: 40%;"></i>
+                        <div style="margin-left: 10px; line-height: 1;">
+                            <h5 style="margin-bottom: 0;">Great Support 24/7</h5>
+                            <span class="text-muted">Instant access to Contact.</span>
+                        </div>                          
+                    </div>
+                    <div style="display: flex; align-items: center;  margin-top: 10px;">
+                        <i class="fa-regular fa-star" style="font-size: 30px; background-color: rgba(76, 160, 212, 0.10); padding: 13px; color: #0C66A6; border-radius: 40%;"></i>
+                        <div style="margin-left: 10px; line-height: 1;">
+                            <h5 style="margin-bottom: 0;">100% Sucure Payment</h5>
+                            <span class="text-muted">We ensure your money is save.</span>
+                        </div>                          
+                    </div>
+                    <div style="display: flex; align-items: center;">
+                        <i class="fa-solid fa-truck" style="font-size: 30px;  margin-top: 10px;  background-color: rgba(76, 160, 212, 0.10); padding: 13px; color: #0C66A6;  border-radius: 40%;"></i>
+                        <div style="margin-left: 10px; line-height: 1;">
+                            <h5 style="margin-bottom: 0;">100% Organic food</h5>
+                            <span class="text-muted">100% healthy & Fresh food.</span>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="alert alert-info mt-5 text-center" role="alert" style="margin: 20%">
-            If your question isn't covered here, you can click <a href="#" class="alert-link"> here. </a> Feel free to email us for further support.
-          </div>
         </div>
-
-    </form>
+    </div>
+</form>
 @endsection
