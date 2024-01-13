d1a353f61ee140376fffe5db18953fd0e6c64439
石田沙有里 <a0r1ng0@icloud.com>
2024-01-13 12:28:51 +0900

    [UI/admin/notifications] Changing stylizes
    - Change pagination and button styles of public/css/style.css
    - Delete poppins fonts styles of public/css/style.css because we decided not to user poppins.
    - Change pagination and button styles of notification.blade.php
    - Change pagination style of usermanagement.blade.php

--- resources/views/admin/notification.blade.php
+++ resources/views/admin/notification.blade.php
@@ -3,17 +3,21 @@
 @section('title', 'Admin: notification')
 
 @section('content')
-    <div class="justify-content-center rounded border" style="min-width:696px">
+    <div class="justify-content-center rounded border">
         <h1 class="ps-3 mt-2">Notification</h1>
-        <form method="POST" action="" accept-charset="UTF-8" class="notifivation_input m-2 border-bottom rounded-0 py-4">
+
+        {{-- Notification Writing Area --}}
+        <form method="POST" action="#" accept-charset="UTF-8" class="m-2 border-bottom rounded-0 py-4">
             @csrf
 
             <label for="message"></label>
-            <textarea name="message" rows="3" id="message" placeholder="Input message"
+            <textarea name="notification" rows="3" id="notification" placeholder="Input message"
                 style="width: calc(100% - 192px - 10px)"></textarea>
-            <input type="submit" value="Add New" class="notification_submit">
+            <input type="submit" value="Add New" class="btn btn-primary">
         </form>
-        <div class="card mx-2 mt-2 border-0 border-bottom  rounded-0">
+
+        {{-- Notification List by Card style --}}
+        <div class="card mx-2 mt-2 border-0 border-bottom rounded-0">
             <div class="card-body">
                 <h5 class="card-title">A system failure has occurred.</h5>
                 <p class="card-text text-secondary">2023.12.20 21:40</p>
@@ -25,11 +29,13 @@
                 <p class="card-text text-secondary">2023.12.20 21:40</p>
             </div>
         </div>
+
+        {{-- Pagination --}}
         <div class="d-flex justify-content-center mt-2">
-            <nav aria-label="Page navigation example">
+            <nav aria-label="Page navigation notifications">
                 <ul class="pagination">
                     <li class="page-item">
-                        <a class="page-link page-arrow-disabled" disabled href="#" aria-label="Previous">
+                        <a class="page-link border disabled" href="#" aria-label="Previous">
                             <span aria-hidden="true">&laquo;</span>
                         </a>
                     </li>
@@ -37,7 +43,7 @@
                     <li class="page-item"><a class="page-link" href="#">2</a></li>
                     <li class="page-item"><a class="page-link" href="#">3</a></li>
                     <li class="page-item">
-                        <a class="page-link page-arrow" href="#" aria-label="Next">
+                        <a class="page-link border" href="#" aria-label="Next">
                             <span aria-hidden="true">&raquo;</span>
                         </a>
                     </li>
