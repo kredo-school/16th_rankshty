33aedbaaa053fa983231a5ced1d19d277daa1236
Nobuaki Yagi <m.yaginobuaki@gmail.com>
2024-01-20 17:51:45 +0900

    -Put SellerController in Seller file, Controllers and "use App\Http\Controllers\Controller;" -Put BuyerController in Buyer file, Controllers and "use App\Http\Controllers\Controller;" -Put AdminController in Admin file, Controllers and "use App\Http\Controllers\Controller;"
    [UI/buyers/review-page]
    -Change the file name, reviewspage.blade.php to reviewpage.blade.php
    
    web.php
    -changed URI of /buyer/reviewspage to /buyer/reviewpage in routing
    -changed method name of show to reviewPage, naming routes as reviewPage in routing
    -changed URI of /seller/index to /seller/my-page and method of index to myPage in routing
    -changed URI of /buyer/index to /buyer/my_page and method of index to myPage in routing
    -added file name of of seller in "use App\Http\Controllers\Seller\SellerController;".
    -added file name of of Admin in "use App\Http\Controllers\Admin\AdminController;"
    -added file name of of Users in "use App\Http\Controllers\Admin\UsersController;"

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\BuyerController;
-use App\Http\Controllers\SellerController;
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\Admin\NotificationsController;
 use App\Http\Controllers\Admin\FollowerFollowingListController;
@@ -26,30 +26,29 @@ use App\Http\Controllers\Admin\FollowerFollowingListController;
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::get('/sellers/guide', [SellerController::class, 'guide'])->name('guide');
-
-Route::get('/buyer/index', [BuyerController::class, 'index'])->name('index');
-
-Route::get('/buyer/reviews', [BuyerController::class, 'show'])->name('reviews');
-
-Route::get('/seller/index', [SellerController::class, 'index'])->name('index');
-
-Route::get('/admin/contentmanagement', [AdminController::class, 'contentManagement'])->name('admin.contentmanagement');
-
-Route::get('/admin/contentmanagementdetails', [AdminController::class, 'contentManagementDetails'])->name('admin.contentmanagementdetails');
-
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//Seller Routes
+Route::get('/sellers/guide', [SellerController::class, 'guide'])->name('guide');
+Route::get('/seller/my-page', [SellerController::class, 'myPage'])->name('myPage');
+
 //Buyer Routes
+Route::get('/buyer/my-page', [BuyerController::class, 'myPage'])->name('myPage');
+Route::get('/buyer/reviewpage', [BuyerController::class, 'reviewPage'])->name('reviewPage');
+
 //FollowBlock
 Route::get('buyer/followblock', [App\Http\Controllers\Buyer\FollowBlockController::class, 'index'])->name('followblock');
 
 //Admin Routes
+Route::get('/admin/contentmanagement', [AdminController::class, 'contentManagement'])->name('admin.contentmanagement');
+Route::get('/admin/contentmanagement/details', [AdminController::class, 'contentManagementDetails'])->name('admin.contentmanagementdetails');
+
 //Notifications
 Route::get('admin/notification', [NotificationsController::class, 'index'])->name('notifications');
+
 //User Management
 Route::get('admin/usermanagement', [UsersController::class, 'index'])->name('users');
+
 //FollowerFolloingList
 Route::get('admin/followerfollowinglist', [FollowerFollowingListController::class, 'index'])->name('followerfollowinglist');
 
