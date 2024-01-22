5cc7bc91cb9f6a068a05f6c7882caba0cbd80504
石田沙有里 <a0r1ng0@icloud.com>
2024-01-22 21:55:26 +0900

    [UI/searchcondition] - Add searchcondition modal
    Additions:
    - Add views/modals/searchcondition.blade.php
    - Add button for access to searchcondition.blade.php in views/home.blade.php

---
+++ resources/views/modals/searchcondition.blade.php
@@ -0,0 +1,136 @@
+<style>
+    .sarchcondition {
+        max-width: 90%;
+    }
+</style>
+
+<!-- Modal -->
+<div class="modal fade " id="searchConditionModal" tabindex="-1" aria-labelledby="searchConditionModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog sarchcondition" style="width: 1500px">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="searchConditionModalLabel">Serach Conditions</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="d-flex justify-content-around">
+                    <div>
+                        <h2>Main Categories</h2>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                                Main Categories 1
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                id="flexRadioDefault2" checked>
+                            <label class="form-check-label" for="flexRadioDefault2">
+                                Main Categories 2
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                id="flexRadioDefault3">
+                            <label class="form-check-label" for="flexRadioDefault3">
+                                Main Categories 3
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                id="flexRadioDefault4">
+                            <label class="form-check-label" for="flexRadioDefault4">
+                                Main Categories 4
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                id="flexRadioDefault5">
+                            <label class="form-check-label" for="flexRadioDefault5">
+                                Main Categories 5
+                            </label>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Sub Categories</h2>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="subFlexRadioDefault"
+                                id="subFlexRadioDefault1">
+                            <label class="form-check-label" for="subFlexRadioDefault1">
+                                Sub Categories 1
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="subFlexRadioDefault"
+                                id="subFlexRadioDefault2" checked>
+                            <label class="form-check-label" for="subFlexRadioDefault2">
+                                Sub Categories 2
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="subFlexRadioDefault"
+                                id="subFlexRadioDefault3">
+                            <label class="form-check-label" for="subFlexRadioDefault3">
+                                Sub Categories 3
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="subFlexRadioDefault"
+                                id="subFlexRadioDefault4">
+                            <label class="form-check-label" for="subFlexRadioDefault4">
+                                Sub Categories 4
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="subFlexRadioDefault"
+                                id="subFlexRadioDefault5">
+                            <label class="form-check-label" for="subFlexRadioDefault5">
+                                Sub Categories 5
+                            </label>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Shippment fee</h2>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="ShppmentFlexRadioDefault"
+                                id="ShppmentFlexRadioDefault1">
+                            <label class="form-check-label" for="ShppmentFlexRadioDefault1">
+                                All
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="ShppmentFlexRadioDefault"
+                                id="ShppmentFlexRadioDefault2" checked>
+                            <label class="form-check-label" for="ShppmentFlexRadioDefault2">
+                                Free Shipping
+                            </label>
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Rating</h2>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="ShppmentFlexRadioDefault"
+                                id="ShppmentFlexRadioDefault1">
+                            <label class="form-check-label" for="ShppmentFlexRadioDefault1">
+                                All
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="ShppmentFlexRadioDefault"
+                                id="ShppmentFlexRadioDefault2" checked>
+                            <label class="form-check-label" for="ShppmentFlexRadioDefault2">
+                                Free Shipping
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Search</button>
+            </div>
+        </div>
+    </div>
+</div>
