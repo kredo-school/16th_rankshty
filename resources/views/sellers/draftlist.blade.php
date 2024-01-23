5cbb1257d81fb189a92855688aa22cbe24cdd7be
1891kaoritanaka <adem_9s@infoseek.jp>
2024-01-23 21:54:56 +0900

    [UI/seller/draftlist-page] - matched the layout to other pages.

--- resources/views/sellers/draftlist.blade.php
+++ resources/views/sellers/draftlist.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
-@section('title','draftlist')
+@section('title', 'draftlist')
 
 @section('content')
-    <h2 class="ps-5">Draft List</h2>
-
-    <table class="table table-hover align-middle">
-        <thead class="table-secondary text-uppercase">
-            <tr>
-                <th>id</th>
-                <th>name</th>
-                <th>price</th>
-                <th>category</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            {{-- @forelse ( $all_products as $product ) --}}
+    <div class="justify-content-center rounded border" style="min-width:696px">
+        <h1 class="ps-3 mt-2">Draft List</h1>
+        <table class="table table-hover align-migddle bg-white text-secondary text-center">
+            <thead class="small table-secondary text-secondary text-uppercase">
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>price</th>
+                    <th>category</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {{-- @forelse ( $all_products as $product ) --}}
                 <tr>
                     <td>1</td>
                     <td>iPhone</td>
@@ -29,29 +29,33 @@
                         <a href="#" class="text-decoration-none fw-bold" style="color: #071657">View Details</a>
                     </td>
                 </tr>
-            {{-- @empty
-                <div class="bg-secondary-subtle">
-                    <p>You don't have a product.</p>
-                </div>
-            @endforelse --}}
-        </tbody>
-    </table>
-    {{-- paginate(10) image --}}
-    <nav aria-label="Page navigation example">
-        <ul class="pagination justify-content-center">
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link focus" href="#">2</a></li>
-            <li class="page-item"><a class="page-link hover" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link page-arrow" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+                {{-- @empty
+                    <div class="bg-secondary-subtle">
+                        <p>You don't have a product.</p>
+                    </div>
+                @endforelse --}}
+            </tbody>
+        </table>
+
+        <div class="d-flex justify-content-center">
+            <nav aria-label="Page navigation usermanagement">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link border disabled" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li class="page-item checked"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link border" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
 @endsection
