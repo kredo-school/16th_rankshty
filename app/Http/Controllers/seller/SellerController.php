7516b52db6bd810da07c972c53010414e364ee70
Nobuaki Yagi <m.yaginobuaki@gmail.com>
2024-01-24 22:31:59 +0900

    [UI/seller/productregistry]
    Additions
    -added productRegistry to routes
    -added productRegistry to SellerController

---
+++ app/Http/Controllers/seller/SellerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SellerController extends Controller
+{
+
+    public function guide()
+    {
+        return view('users.sellers.guide_page');
+
+    }
+
+    public function myPage()
+    {
+        return view('sellers.index');
+    }
+
+    public function productRegistry()
+    {
+        return view('sellers.productRegistry');
+    }
+}
