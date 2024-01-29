cc26ae58ff148516b6dbf4d6eb87f3eab6536d4d
石田沙有里 <a0r1ng0@icloud.com>
2024-01-29 23:15:36 +0900

    [UI/register] - Create Register page
    Addtions:
    - add eyeIcon_confirm click event in password.js
    - add link to js/password.js in register.blade.php
    - add icon in password input form in register.blade.php
    
    Changes:
    - change form checkout label text

--- public/js/password.js
+++ public/js/password.js
@@ -1,4 +1,4 @@
-// eyeIconのclickクリックイベント
+// eyeIcon click event
 $("#eyeIcon").on("click", () => {
     // eyeIconのclass切り替え
     $("#eyeIcon").toggleClass("fa-eye-slash fa-eye");
@@ -7,5 +7,17 @@ $("#eyeIcon").on("click", () => {
         $("#password").attr("type", "text");
     } else {
         $("#password").attr("type", "password");
+    }
+});
+
+// eyeIcon_confirm click event
+$("#eyeIcon_confirm").on("click", () => {
+    // eyeIconのclass切り替え
+    $("#eyeIcon_confirm").toggleClass("fa-eye-slash fa-eye");
+    // inputのtype切り替え
+    if ($("#password-confirm").attr("type") == "password") {
+        $("#password-confirm").attr("type", "text");
+    } else {
+        $("#password-confirm").attr("type", "password");
     }
 });
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('title', 'Create Account')
+
+@push('js')
+    <script src="{{ asset('js/password.js') }}" defer></script>
+@endpush
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -38,12 +44,13 @@
                                     @enderror
                                 </div>
                             </div>
-
                             <div class="row mb-3">
-                                <div class="col-md-12">
+                                <div class="col-md-12 position-relative">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password" placeholder="Password">
+                                    <i id="eyeIcon"
+                                        class="fa-regular fa-eye-slash position-absolute translate-middle top-50 end-0 me-2"></i>
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -52,29 +59,33 @@
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <div class="col-md-12">
+                                <div class="col-md-12 position-relative">
                                     <input id="password-confirm" type="password" class="form-control"
                                         name="password_confirmation" required autocomplete="new-password"
                                         placeholder="Comfirm Password">
+                                    <i id="eyeIcon_confirm"
+                                        class="fa-regular fa-eye-slash position-absolute translate-middle top-50 end-0 me-2"></i>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="isSeller" id="isSeller"
                                     {{ old('isSeller') ? 'checked' : '' }}>
-
                                 <label class="form-check-label" for="isSeller">
-                                    {{ __('Are you a Seller?') }}
+                                    {{ __('Are You A Seller?') }}
                                 </label>
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="accepet" id="accepet"
                                     {{ old('accepet') ? 'checked' : '' }}>
-
                                 <label class="form-check-label" for="accepet">
-                                    {{ __('Accept all terms & conditions') }}
+                                    {{ __('Accept All Terms & Conditions') }}
                                 </label>
                             </div>
-
                             <div class="row py-2">
                                 <div class="col-md-12">
                                     <button type="submit" class="btn btn-primary" style="width: 100%;">
