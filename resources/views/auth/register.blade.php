e76e8d8ad3079b1cc2dc8c86cf09267ecd768562
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-01 14:33:03 +0900

    just commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.app')
+
+@section('title', 'Create Account')
+
+@push('js')
+    <script src="{{ asset('js/password.js') }}" defer></script>
+@endpush
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header bg-white text-center border-0">
+                        <h1>{{ __('Create Account') }}</h1>
+                    </div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                        value="{{ old('name') }}" required autocomplete="name" autofocus
+                                        placeholder="User Name">
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-md-12">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" placeholder="E-mail">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-12 position-relative">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="new-password" placeholder="Password">
+                                    <i id="eyeIcon"
+                                        class="fa-regular fa-eye-slash position-absolute translate-middle top-50 end-0 me-2"></i>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-12 position-relative">
+                                    <input id="password-confirm" type="password" class="form-control"
+                                        name="password_confirmation" required autocomplete="new-password"
+                                        placeholder="Comfirm Password">
+                                    <i id="eyeIcon_confirm"
+                                        class="fa-regular fa-eye-slash position-absolute translate-middle top-50 end-0 me-2"></i>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="isSeller" id="isSeller"
+                                    {{ old('isSeller') ? 'checked' : '' }}>
+                                <label class="form-check-label" for="isSeller">
+                                    {{ __('Are You A Seller?') }}
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="accepet" id="accepet"
+                                    {{ old('accepet') ? 'checked' : '' }}>
+                                <label class="form-check-label" for="accepet">
+                                    {{ __('Accept All Terms & Conditions') }}
+                                </label>
+                            </div>
+                            <div class="row py-2">
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-primary" style="width: 100%;">
+                                        {{ __('Create Account') }}
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="row my-2 justify-content-center">
+                                <div class="col-md-8 text-center">
+                                    {{ __('Do you have an addount already?') }}
+                                    <a href="{{ route('login') }}">
+                                        {{ __('Login') }}
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Admin\NotificationsController;
+use App\Http\Controllers\Admin\FollowerFollowingListController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Buyer\FollowBlockController;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Auth::routes();
+
+//Home Routes
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+//Seller Routes
+Route::get('/seller/guide', [SellerController::class, 'guide'])->name('seller.guide');
+Route::get('/seller/my-page', [SellerController::class, 'myPage'])->name('seller.myPage');
+Route::get('/seller/product-registry', [SellerController::class, 'productRegistry'])->name('seller.produtRegisty');
+Route::get('/seller/draftlist', [SellerController::class, 'draftlist'])->name('seller.draftlist');
+Route::get('/seller/productslist', [SellerController::class, 'productsList'])->name('seller.productslist');
+
+//Buyer Routes
+Route::get('/buyer/my-page', [BuyerController::class, 'myPage'])->name('buyer.myPage');
+Route::get('/buyer/reviews-page', [BuyerController::class, 'reviewsPage'])->name('buyer.reviewsPage');
+Route::get('/buyer/service', [BuyerController::class, 'service'])->name('seller.service');
+Route::get('/buyer/help', [BuyerController::class, 'help'])->name('buyer.help');
+Route::get('/buyer/service', [BuyerController::class, 'service'])->name('buyer.service');
+Route::get('/buyer/contact', [BuyerController::class, 'contact'])->name('buyer.contact');
+Route::get('/buyer/returnproducts', [BuyerController::class, 'returnProducts'])->name('buyer.returnproducts');
+
+//FollowBlock
+Route::get('/buyer/followblock', [FollowBlockController::class, 'index'])->name('buyer.followblock');
+
+
+//Admin Routes
+//Content Management
+Route::get('/admin/contentmanagement', [AdminController::class, 'contentManagement'])->name('admin.contentmanagement');
+Route::get('/admin/contentmanagement/details', [AdminController::class, 'contentManagementDetails'])->name('admin.contentmanagementdetails');
+//Notifications
+Route::get('/admin/notification', [NotificationsController::class, 'index'])->name('admin.notifications');
+//User Management
+Route::get('/admin/usermanagement', [UsersController::class, 'index'])->name('admin.users');
+//FollowerFolloingList
+Route::get('/admin/followerfollowinglist', [FollowerFollowingListController::class, 'index'])->name('admin.followerfollowinglist');
+//Ranking
+Route::get('/admin/ranking', [AdminController::class, 'ranking'])->name('admin.ranking');
+//Dashboard
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+//category
+Route::get('/admin/category', [AdminController::class, 'category'])->name('admin.category');
+//complainhandling
+Route::get('/admin/complainhandling', [AdminController::class, 'complainhandling'])->name('admin.complainhandling');
+Route::get('/admin/complaindetail', [AdminController::class, 'complaindetail'])->name('admin.complaindetail');
+
+
