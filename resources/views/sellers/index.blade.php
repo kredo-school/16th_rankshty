73a27d067982dbda547ec596ff4b019072813741
石田沙有里 <a0r1ng0@icloud.com>
2024-02-03 19:01:32 +0900

    [UI/seller/editprofile] - create sellers/editprofile modal
    Additions:
    - add sellsers/modal/editprofile.blade.php
    - add "Company Name" display area in sellers/index.blade.php

--- resources/views/sellers/index.blade.php
+++ resources/views/sellers/index.blade.php
@@ -3,85 +3,100 @@
 @section('title', 'Buyer: My page')
 
 @section('content')
-<div class="container mt-4 col-6">
-    <div class="container-header border border-tertiary rounded-top px-3 py-1">
-        <h1>Account Infomation</h1>
-    </div>
-    <div class="container-body border border-top-0 rounded-bottom border-tertiary px-3">
-        <div class="row">
-            <div class="col-8 mt-2">
-                <div class="row">
-                    <div class="col-6">
-                        <p class="mb-0">First Name</p>
-                        <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Emily</p>
+    <div class="container mt-4 col-6">
+        <div class="container-header border border-tertiary rounded-top px-3 py-1">
+            <h1>Account Infomation</h1>
+        </div>
+        <div class="container-body border border-top-0 rounded-bottom border-tertiary px-3">
+            <div class="row">
+                <div class="col-8 mt-2">
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="mb-0">First Name</p>
+                            <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Emily</p>
+                        </div>
+                        <div class="col-6">
+                            <p class="mb-0">Last Name</p>
+                            <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Jones</p>
+                        </div>
                     </div>
-                    <div class="col-6">
-                        <p class="mb-0">Last Name</p>
-                        <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Jones</p>
+
+                    <div class="col-12">
+                        <p class="mb-0">Username</p>
+                        <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">EmilyJones22</p>
+                    </div>
+
+                    <div class="col-12">
+                        <p class="mb-0">Company Name</p>
+                        <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">NovaLink Global</p>
+                    </div>
+
+                    <div class="col-12">
+                        <p class="mb-0">Email</p>
+                        <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">emily-jones58@gmail.com</p>
+                    </div>
+
+                    <div class="col-12">
+                        <p class="mb-0">Phone Number</p>
+                        <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">(603) 555-0123</p>
                     </div>
                 </div>
 
-                <div class="col-12">
-                    <p class="mb-0">Username</p>
-                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">EmilyJones22</p>
-                </div>
-
-                <div class="col-12">
-                    <p class="mb-0">Email</p>
-                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">emily-jones58@gmail.com</p>
-                </div>
-
-                <div class="col-12">
-                    <p class="mb-0">Phone Number</p>
-                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">(603) 555-0123</p>
+                <div class="col-4">
+                    <img src="#" alt="" class="rounded-circle picture-circle border border-primary">
                 </div>
             </div>
 
-            <div class="col-4">
-                <img src="#" alt="" class="rounded-circle picture-circle border border-primary">
+            <div class="row">
+                <div class="col-12">
+                    <p class="mb-0">Street Address</p>
+                    <p class="rounded py-2 px-3" style="background-color: #E6E6E6;">4140 Parl</p>
+                </div>
+
+                <div class="col-4">
+                    <p class="mb-0">Country / Region</p>
+                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">United States</p>
+                </div>
+                <div class="col-4">
+                    <p class="mb-0">States</p>
+                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Washinton DC</p>
+                </div>
+                <div class="col-4">
+                    <p class="mb-0">Zip Code</p>
+                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">20033</p>
+                </div>
+
+                <div class="col-6">
+                    <p class="mb-0">password</p>
+                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">**********</p>
+                </div>
+
+                <div class="col-6">
+                    <p class="mb-0">Credit Card Number</p>
+                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">***************</p>
+                </div>
+
+                <div class="col-12">
+                    <p class="mb-0">Add review</p>
+                    <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Lorem ipsum dolor sit amet. Ut
+                        rerum asperiores et eveniet mollitia sit optio repellendus non minima quam vel obcaecati eius sed
+                        repudiandae tempora non dicta unde. Et dignissimos recusandae ut sint blanditiis et natus dolores
+                        qui facere suscipit? Eos voluptates eveniet et aliquam nesciunt vel excepturi enim aut adipisci
+                        culpa ut distinctio quia.
+                        Et quia sapiente eos rerum accusantium et distinctio eveniet. Ab quos accusantium quo cupiditate
+                        iure aut rerum ipsa aut quia doloribus ut dolorem nihil et laborum eius aut fuga tempore. Non autem
+                        odit ut quod quos non corrupti quas et odit quas in eaque magni. In vero amet et ipsum sunt eum
+                        alias dicta cum dolorem magni non debitis reiciendis.
+                        Aut quis omnis non voluptatem assumenda quo earum galisum cum ipsum corrupti ea tempora repellendus.
+                        Eos debitis dolore At magnam dolor aut nostrum veniam et magnam praesentium aut incidunt quia.
+                    </p>
+                </div>
+                <div class="text-end mb-3">
+                    <button type="button" class="btn btn-primary rounded-3" data-bs-toggle="modal"
+                        data-bs-target="#editSellerProfile">Edit</button>
+                </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-12">
-                <p class="mb-0">Street Address</p>
-                <p class="rounded py-2 px-3" style="background-color: #E6E6E6;">4140 Parl</p>
-            </div>
-
-            <div class="col-4">     
-                <p class="mb-0">Country / Region</p>
-                <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">United States</p>
-            </div>
-            <div class="col-4">  
-                <p class="mb-0">States</p>
-                <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Washinton DC</p>
-            </div>
-            <div class="col-4">  
-                <p class="mb-0">Zip Code</p>
-                <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">20033</p>
-            </div>
-
-            <div class="col-6">
-                <p class="mb-0">password</p>
-                <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">**********</p>
-            </div>
-
-            <div class="col-6">
-                <p class="mb-0">Credit Card Number</p>
-                <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">***************</p>
-            </div>
-
-            <div class="col-12">
-                <p class="mb-0">Add review</p>
-                <p class="rounded col-12 py-2 px-3" style="background-color: #E6E6E6;">Lorem ipsum dolor sit amet. Ut rerum asperiores et eveniet mollitia sit optio repellendus non minima quam vel obcaecati eius sed repudiandae tempora non dicta unde. Et dignissimos recusandae ut sint blanditiis et natus dolores qui facere suscipit? Eos voluptates eveniet et aliquam nesciunt vel excepturi enim aut adipisci culpa ut distinctio quia.
-                    Et quia sapiente eos rerum accusantium et distinctio eveniet. Ab quos accusantium quo cupiditate iure aut rerum ipsa aut quia doloribus ut dolorem nihil et laborum eius aut fuga tempore. Non autem odit ut quod quos non corrupti quas et odit quas in eaque magni. In vero amet et ipsum sunt eum alias dicta cum dolorem magni non debitis reiciendis.
-                    Aut quis omnis non voluptatem assumenda quo earum galisum cum ipsum corrupti ea tempora repellendus. Eos debitis dolore At magnam dolor aut nostrum veniam et magnam praesentium aut incidunt quia.
-                </p>
-            </div>
-            <div class="text-end mb-3">
-                <button type="button" class="btn btn-primary rounded-3">Edit</button>
-            </div>
-        </div>
     </div>
-</div>
+    @include('sellers.modal.editprofile')
 @endsection
---
+++ resources/views/sellers/modal/editprofile.blade.php
@@ -0,0 +1,98 @@
+<!-- Modal -->
+<div class="modal fade" id="editSellerProfile" tabindex="-1" aria-labelledby="editSellerProfile" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content text-center">
+            <div class="d-flex flex-row-reverse me-2 mt-2">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="container mt-4 col-10">
+                <div class="container-header border border-tertiary rounded-top px-3 py-1">
+                    <h1>Account Infomation</h1>
+                </div>
+                <div class="container-body border border-top-0 rounded-bottom border-tertiary px-3 text-start">
+                    <div class="row">
+                        <div class="col-8 mt-2">
+                            <div class="row">
+                                <div class="col-6">
+                                    <label for="firstName" class="form-label mb-0 mt-2">First Name</label>
+                                    <input type="text" class="form-control" id="firstName" value="Emily">
+                                </div>
+                                <div class="col-6">
+                                    <label for="lastName" class="form-label mb-0 mt-2">Last Name</label>
+                                    <input type="text" class="form-control" id="lastName" value="Jones">
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <label for="username" class="form-label mb-0 mt-2">Username</label>
+                                <input type="text" class="form-control" id="username" value="EmilyJones22">
+                            </div>
+
+                            <div class="col-12">
+                                <label for="companyName" class="form-label mb-0 mt-2">Company Name</label>
+                                <input type="text" class="form-control" id="companyName" value="NovaLink Global">
+                            </div>
+
+                            <div class="col-12">
+                                <label for="email" class="form-label mb-0 mt-2">Email</label>
+                                <input type="email" class="form-control" id="email"
+                                    value="emily-jones58@gmail.com">
+                            </div>
+
+                            <div class="col-12">
+                                <label for="phoneNumber" class="form-label mb-0 mt-2">Phone Number</label>
+                                <input type="tel" class="form-control" id="phoneNumber" value="(603) 555-0123">
+                            </div>
+                        </div>
+
+                        <div class="col-4 text-center">
+                            <img src="https://via.placeholder.com/150" alt=""
+                                class="rounded-circle picture-circle border border-primary mt-2">
+                            <input type="file" class="form-control mt-2" id="picture" value="Change Picture">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <label for="streetAddress" class="form-label mb-0 mt-2">Street Address</label>
+                            <input type="text" class="form-control" id="streetAddress" value="4140 Parl">
+                        </div>
+                        <div class="col-4">
+                            <label for="country" class="form-label mb-0 mt-2">Country / Region</label>
+                            <input type="text" class="form-control" id="country" value="United States">
+                        </div>
+                        <div class="col-4">
+                            <label for="states" class="form-label mb-0 mt-2">States</label>
+                            <input type="text" class="form-control" id="states" value="Washinton DC">
+                        </div>
+                        <div class="col-4">
+                            <label for="zipCode" class="form-label mb-0 mt-2">Zip Code</label>
+                            <input type="text" class="form-control" id="zipCode" value="20033">
+                        </div>
+
+                        <div class="col-6">
+                            <label for="password" class="form-label mb-0 mt-2">Password</label>
+                            <input type="text" class="form-control" id="password" value="123456">
+                        </div>
+
+                        <div class="col-6">
+                            <label for="creditCard" class="form-label mb-0 mt-2">Credit Card Number</label>
+                            <input type="text" class="form-control" id="creditCard" value="123456">
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <label for="review" class="form-label mb-0 mt-2">Add review</label>
+                        <textarea class="rounded col-12" id="review" placeholder="Input Review">test</textarea>
+                    </div>
+
+                    <div class="d-flex flex-row-reverse gap-2">
+                        <button type="button" class="btn btn-primary rounded-3">Save Change</button>
+                        <button type="button" class="btn btn-secondary rounded-3 me-2"
+                            data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
