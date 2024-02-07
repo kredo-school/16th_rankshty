d932fd5f6a744a3d2d7402feef4b71ff0feb8aa0
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-07 21:54:28 +0900

    [UI/seller/review] - Added something
    Added:
    - Page number

--- resources/views/users/seller/reviews.blade.php
+++ resources/views/users/seller/reviews.blade.php
@@ -218,8 +218,26 @@
                     <button type="button" class="btn btn-outline-secondary">Helphul</button>
                     <button type="button" class="btn btn-light ms-3 text-muted">Report</button>
                 </div>
-                
             </div>        
+            <div class="d-flex justify-content-center">
+                <nav aria-label="Page navigation usermanagement">
+                    <ul class="pagination">
+                        <li class="page-item">
+                            <a class="page-link border disabled" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="page-item checked"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link border" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
     </div>
 </div>
 @endsection
