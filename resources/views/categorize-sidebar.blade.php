4f94354ee204b981576fc9ef3ddfec84fdb37f41
1891kaoritanaka <adem_9s@infoseek.jp>
2024-02-07 23:04:50 +0900

    [UI/buyer/categorize-page] - creating categorize page

---
+++ resources/views/categorize-sidebar.blade.php
@@ -0,0 +1,55 @@
+<div class="col-3 categorize-bar">
+    <div class="mb-4">
+        <h3>Main Categories</h3>
+        <nav class="nav flex-column">
+            <a href="#" class="form-check">
+                <input class="form-check-input" type="radio" value="" name="mainCheck" id="defaultCheck1">
+                <label class="form-check-label text-black" for="defaultCheck1">Fasion(500)</label>
+            </a>
+            <a href="#" class="form-check">
+                <input class="form-check-input" type="radio" value="" name="mainCheck" id="defaultCheck2">
+                <label class="form-check-label text-black" for="defaultCheck2">Sports(300)</label>
+            </a>
+            <a href="#" class="form-check">
+                <input class="form-check-input" type="radio" value="" name="mainCheck" id="defaultCheck3">
+                <label class="form-check-label text-black" for="defaultCheck3">Food(200)</label>
+            </a>
+        </nav>
+    </div>
+
+    <div class="mb-4">
+        <h4>Sub categories</h4>
+        <nav class="nav flex-column">
+            <a href="#" class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="subCheck1">
+                <label class="form-check-label text-black" for="subCheck1">Tops(300)</label>
+            </a>
+            <a href="#" class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="subCheck2">
+                <label class="form-check-label text-black" for="subCheck2">Bottoms(100)</label>
+            </a>
+            <a href="#" class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="subCheck3">
+                <label class="form-check-label text-black" for="subCheck3">Inner(100)</label>
+            </a>
+        </nav>
+    </div>
+
+    <div class="mb-4">
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+                <h4 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                        Price
+                    </button>
+                </h4>
+                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                    <div class="accordion-body">
+                        <strong></strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
