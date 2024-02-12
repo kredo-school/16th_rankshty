34143799bb1dbba021d3e3b38fd43cf292bb231a
hanna-ht <hana.aug12@gmail.com>
2024-02-12 15:06:02 +0900

    [UI/buyers/information page] Update Update information.blade.php

--- resources/views/buyers/information.blade.php
+++ resources/views/buyers/information.blade.php
@@ -3,35 +3,21 @@
 @section('title', 'Information')
 
 @section('content')
-<style>
-    .border-orange {
-        color: #FA8557;
-    }
-    .text-orange {
-        color: #FA8557;
-    }
-</style>
 
-<head>
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-</head>
-
-<body>
-    <div class="row justify-content-center">
-        <h1 class="text-center">Information</h1>
+<div class="row justify-content-center">
+    <h1 class="text-center">Information</h1>
+</div>
+<div class="row">
+    <div class="col-2 text-center">
+        <h4 class="border-bottom border-thick border-orange" style="color: #FA8557; ,border-bottom; 1rem solid #FA8557;">Information</h4>
     </div>
-    <div class="row">
-        <div class="col-2">
-            <h4 class="border-bottom border-orange text-orange">Information</h4>
-        </div>
-        <div class="col-2">
-            <h4>Notification</h4>
-        </div>
+    <div class="col-2 text-center">
+        <h4>Notification</h4>
     </div>
-    <div class="row border-bottom">
-        <span>[12/20 21:40 Update] A system failure has occured.</span>
-        <span class="text-secondary">2023/12/20 21:40</span>
-    </div>
-</body>
+</div>
+<div class="row border-bottom">
+    <span>[12/20 21:40 Update] A system failure has occured.</span>
+    <span class="text-secondary">2023/12/20 21:40</span>
+</div>
 
 @endsection
