292971be2b58b43c0fe97b388d918fd521a579e7
hanna-ht <hana.aug12@gmail.com>
2024-02-12 14:24:41 +0900

    [UI/buyers/purchase-history page] Update Add purchasehistory.css file Update style.css file, purchase-history.blade.php

--- resources/views/buyers/purchase-history.blade.php
+++ resources/views/buyers/purchase-history.blade.php
@@ -5,22 +5,7 @@
 @section('content')
 
 <head>
-<style>
-    .btn {
-        border-radius: 50vh;
-        height: 50%;
-        width: 50%;
-        font-size: 0.8rem;"
-    }
-    .btn-primary {
-        border-color: #071657;
-        background-color: #071657;
-        color: #ffff;
-    }
-
-</style>
-
-
+    <link rel="stylesheet" href="{{ asset('/css/purchasehistory.css') }}">
 </head>
 
 <body>
@@ -48,7 +33,7 @@
                             
                             <tbody>
                                 <tr>
-                                    <td><img src="{{ asset('img/green pepper.jpeg') }}" style="width: 100px; height: 100px;" alt=""></td>
+                                    <td><img src="{{ asset('img/green pepper.jpeg') }}" class="product-img" alt=""></td>
                                     <td>Green Capsicum</td>
                                     <td>$14.99</td>
                                     <td>1</td>
@@ -57,7 +42,7 @@
                                     </td>
                                     <td>2024/01/10</td>
                                     <td>
-                                        <button class="btn btn-primary btn-sm" type="submit">Write Review</button>
+                                        <button class="btn btn-primary" type="submit">Write Review</button>
                                     </td>
                                     <td>
                                         <button class="btn btn-primary" type="submit">Purchase</button>
@@ -67,8 +52,33 @@
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td><img src="{{ asset('img/chinese cabbage.jpeg') }}" style="width: 100px; height: 100px;" alt=""></td>
+                                    <td><img src="{{ asset('img/chinese cabbage.jpeg') }}" class="product-img" alt=""></td>
                                     <td>Chinese Cabbage</td>
+                                    <td>$45.00</td>
+                                    <td>1</td>
+                                    <td>
+                                        <p class="in-stock">In Stock</p>
+                                    </td>
+                                    <td>2024/01/11</td>
+                                    <td>
+                                        <a href="#">
+                                        <button class="btn btn-primary" type="submit">Write Review</button>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="#">
+                                        <button class="btn btn-primary" type="submit">Purchase</button>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="#">
+                                        <button class="btn btn-primary" type="submit">Return</button>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><img src="{{ asset('img/mango.jpeg') }}" class="product-img" alt=""></td>
+                                    <td>Fresh Mango</td>
                                     <td>$45.00</td>
                                     <td>1</td>
                                     <td>
@@ -77,12 +87,12 @@
                                     <td>2024/01/11</td>
                                     <td>
                                         <a href="#">
-                                        <button class="btn btn-primary r-2" type="submit">Write Review</button>
+                                        <button class="btn btn-primary" type="submit">Write Review</button>
                                         </a>
                                     </td>
                                     <td>
                                         <a href="#">
-                                        <button class="btn btn-primary" type="submit">Purchase</button>
+                                        <button class="btn btn-gray" type="submit">Purchase</button>
                                         </a>
                                     </td>
                                     <td>
