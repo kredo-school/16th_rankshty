2a6df90479d5fae23df3caa91c742d156a806798
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-17 16:33:06 +0900

    [UI/seller/order-detail] - Create Seeder for order-detail
    Added:
    - OrderDetails.php
    - DatabaseSeeder.php
    - OrderDetailsSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(FavoritesSeeder::class);
+        $this->call([
+            OrderDetailsSeeder::class,
+        ]);
     }
 }
