04340f2f5617152deb165e61df98771485ed518f
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-17 19:19:19 +0900

    [UI/seller/order-detail] - Added something

--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -5,7 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class OrderDetails extends Model
+class OrderDetail extends Model
 {
     use HasFactory;
+
+    protected $table = 'order_details';
+
+    protected $fillable = [
+        'order_id',
+        'product_details_id',
+        'quantity',
+        'unitPrice',
+        'discount',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+
+    public function productDetail()
+    {
+        return $this->belongsTo(ProductDetail::class, 'product_details_id');
+    }
 }
