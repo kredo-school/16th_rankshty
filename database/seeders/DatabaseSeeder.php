ce371494faa9fef8912775bc59b4b8267a0cf8c4
1891kaoritanaka <adem_9s@infoseek.jp>
2024-02-19 22:51:55 +0900

    - created Models/MainCategory - created Models/SubCategory - created Models/CategoryProduct

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CartSeeder;
 use Database\Seeders\ProductSeeder;
 use Database\Seeders\SubCategorySeeder;
 use Database\Seeders\ProductDetailSeeder;
