88d762cfd56a059f676febb0572f8989c2845507
石田沙有里 <a0r1ng0@icloud.com>
2024-02-20 21:48:36 +0900

    [UI/common/sidebar] - change Models/Product.php
    - change Models/Product.php

---
+++ database/seeders/FavoriteSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Favorite;
+
+class FavoriteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $favorites = [
+            ['user_id' => 1, 'product_id' => 1],
+            ['user_id' => 1, 'product_id' => 2],
+            ['user_id' => 2, 'product_id' => 1],
+            ['user_id' => 2, 'product_id' => 2],
+            ['user_id' => 3, 'product_id' => 1],
+            ['user_id' => 3, 'product_id' => 2],
+        ];
+
+        foreach ($favorites as $favorite) {
+            Favorite::create($favorite);
+        }
+    }
+}
