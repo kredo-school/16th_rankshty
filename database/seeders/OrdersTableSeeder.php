b52d039c42b0c850a281f5f4eaa9fd118edf74a2
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-20 15:06:24 +0900

    [UI/seller/orders] - crearte seller for orders

--- database/seeders/OrdersTableSeeder.php
+++ database/seeders/OrdersTableSeeder.php
@@ -2,15 +2,30 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Orders; 
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+use App\Models\Orders;
 
-class OrdersTableSeeder extends Seeder
+class OrdersSeeder extends Seeder
 {
-   
-    public function run(): void
+    public function run()
     {
-        Orders::factory(10)->create();
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            DB::table('orders')->insert([
+                'buyer_id'      => $faker->numberBetween(1, 100),
+                'seller_id'     => $faker->numberBetween(1, 100),
+                'address_id'    => $faker->numberBetween(1, 100),
+                'freight'       => $faker->randomFloat(2, 10, 50),
+                'shipped_date'  => $faker->dateTimeThisMonth,
+                'ordered_at'    => $faker->dateTimeThisMonth,
+                'ordered_date'  => $faker->dateTimeThisMonth,
+                'required_date' => $faker->dateTimeThisMonth,
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ]);
+        }
     }
 }
