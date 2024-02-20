e5b9645c82e3b1e7d888ba12d022f6d1f5f06e99
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-20 14:38:56 +0900

    [UI/seller/order-details] - minor change

--- database/seeders/OrderDetailsTableSeeder.php
+++ database/seeders/OrderDetailsTableSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\OrderDetails;
 
-class OrderDetailsTableSeeder extends Seeder
+class OrderDetailsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        OrderDetails::factory(50)->create();
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            \DB::table('order_details')->insert([
+                'order_id'            => $faker->numberBetween(1, 100),
+                'product_details_id'  => $faker->numberBetween(1, 50),
+                'quantity'            => $faker->numberBetween(1, 10),
+                'unitPrice'           => $faker->randomFloat(2, 10, 100),
+                'discount'            => $faker->numberBetween(0, 20),
+                'created_at'          => now(),
+                'updated_at'          => now(),
+            ]);
+        }
     }
 }
