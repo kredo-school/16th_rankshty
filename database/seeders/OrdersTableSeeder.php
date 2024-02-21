b8888eb0026bcb5ec56f181f1d655af6b6966462
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-21 23:43:49 +0900

    [UI/seller/orders] - modify seeder file for orders

--- database/seeders/OrdersTableSeeder.php
+++ database/seeders/OrdersTableSeeder.php
@@ -7,7 +7,7 @@ use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
 use App\Models\Orders;
 
-class OrdersSeeder extends Seeder
+class OrdersTableSeeder extends Seeder
 {
     public function run()
     {
@@ -15,16 +15,14 @@ class OrdersSeeder extends Seeder
 
         foreach (range(1, 10) as $index) {
             DB::table('orders')->insert([
-                'buyer_id'      => $faker->numberBetween(1, 100),
-                'seller_id'     => $faker->numberBetween(1, 100),
-                'address_id'    => $faker->numberBetween(1, 100),
+                'buyer_id'      => $faker->numberBetween(1, 10),
+                'seller_id'     => $faker->numberBetween(1, 10),
+                'address_id'    => $faker->numberBetween(1, 2),
                 'freight'       => $faker->randomFloat(2, 10, 50),
                 'shipped_date'  => $faker->dateTimeThisMonth,
                 'ordered_at'    => $faker->dateTimeThisMonth,
-                'ordered_date'  => $faker->dateTimeThisMonth,
+                'order_date'  => $faker->dateTimeThisMonth,
                 'required_date' => $faker->dateTimeThisMonth,
-                'created_at'    => now(),
-                'updated_at'    => now(),
             ]);
         }
     }
