c802c068e1c793657e83b373a6960f732d8afbbc
Ayaka Suzuki <graphite0505@gmail.com>
2024-02-21 23:35:38 +0900

    [UI/seller/GuidePage] - error correction

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Seller;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SellerController extends Controller
 {
     public function guide()
     {
-        return view('seller.guide');
+        return view('users.seller.guide_page');
     }
 
     public function productsList()
