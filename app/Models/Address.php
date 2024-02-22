eafee1d07b3b76981eefcbc365312d514120e617
Nobuaki Yagi <m.yaginobuaki@gmail.com>
2024-02-22 19:45:06 +0900

    [Models/Address.php] - added relationship with orders Modified [addresses_table.php]

---
+++ app/Models/Address.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'user_id',
+        'post_code',
+        'receiver_name',
+        'address',
+        'city',
+        'prefecture',
+        'phone_number',
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class)->withTrashed();
+    }
+
+    public function orders()
+    {
+        return $this->hasOne(Order::class)->latest();
+    }
+}
