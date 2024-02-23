158de696c1d448f1897e3afff8320218c81d12ff
1891kaoritanaka <adem_9s@infoseek.jp>
2024-02-24 01:30:36 +0900

    - Fixed a strange change in Favorite.php - Add Product.php, which has disappeared from main

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -8,38 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     use HasFactory;
-
     public $timestamps = false;
     protected $fillable = ['user_id','product_id'];
-
-    #A product belongs to a user
-    public function users()
-    {
-        return $this->belongsTo(User::class)->withTrashed();;
-    }
-
-    public function reviews()
-    {
-        return $this->hasMany(Review::class)->latest();
-    }
-
-    public function category_product()
-    {
-        return $this->hasMany(CategoryProduct::class)->latest();
-    }
-
-    public function favorites()
-    {
-        return $this->hasMany(Favorite::class)->latest();
-    }
-
-    public function dms()
-    {
-        return $this->hasMany(DMS::class)->latest();
-    }
-
-    protected $casts = [
-        'status' => ProductStatus::class,
-    ];
-
 }
