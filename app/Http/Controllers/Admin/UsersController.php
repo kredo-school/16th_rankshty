99978083264c46202353c0d794f0444d1324af9a
石田沙有里 <a0r1ng0@icloud.com>
2024-03-13 21:53:41 +0900

    [BE UI/admin/usermanagement] - create pagination BE
    Additions:
    add db connection in UsersController.php
    
    Changes:
    change code from html style to blade style in usermanagement.blade.php

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 
 class UsersController extends Controller
@@ -11,6 +12,7 @@ class UsersController extends Controller
     //
     public function index()
     {
-        return view('admin.usermanagement');
+        $users = User::orderBy('id')->paginate(10);
+        return view('admin.usermanagement', ['users' => $users]);
     }
 }
